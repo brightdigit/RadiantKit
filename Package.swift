7927e5ca94bb7bec8298e03735ca494b3a61c086
Leo Dion <leogdion@brightdigit.com>
2026-01-06 17:17:19 -0500

    fixing download-platform and android build issue with swift-log

--- Package.swift
+++ Package.swift
@@ -56,7 +56,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.6.0"),
   ],
   targets: [
     .target(
